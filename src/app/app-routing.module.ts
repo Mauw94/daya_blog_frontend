77124a829859e04adb22255f4a846068b5ada76c
Maurits Seelen <mauritsseelen@gmail.com>
2018-08-05 15:05:53 +0200

    headers done

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BlogComponent } from './blog/blog.component';
+
+const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'blog', component: BlogComponent }
+];
+
+@NgModule({
+    exports: [RouterModule],
+    imports: [RouterModule.forRoot(routes)]
+})
+export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend';
+
+  constructor() { }
+
+  ngOnInit() {
+    this.changeActiveClass();
+  }
+
+  changeActiveClass(): void {
+    const navbar = document.getElementById('nav-bar');
+    const items = navbar.getElementsByClassName('nav-item');
+
+    for (let i = 0; i < items.length; i++) {
+      items[i].addEventListener('click', function () {
+        const current = document.getElementsByClassName(' active');
+        current[0].className = current[0].className.replace(' active', '');
+        this.className += ' active';
+      });
+    }
+  }
 }
