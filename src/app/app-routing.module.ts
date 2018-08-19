e3c0eb2b05a240bfab5bf78e872e907c11d83bc3
Maurits Seelen <mauritsseelen@gmail.com>
2018-08-19 16:32:37 +0200

    blog post route, some refactoring

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { AboutComponent } from './about/about.component';
 import { StoryComponent } from './story/story.component';
 import { LoginComponent } from './login/login.component';
 import { CreateAccountComponent } from './create-account/create-account.component';
+import { BlogFormComponent } from './blog-form/blog-form.component';
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
@@ -14,7 +15,8 @@ const routes: Routes = [
     { path: 'about', component: AboutComponent },
     { path: 'story', component: StoryComponent },
     { path: 'login', component: LoginComponent },
-    { path: 'create', component: CreateAccountComponent }
+    { path: 'create', component: CreateAccountComponent },
+    { path: 'postblog', component: BlogFormComponent }
 ];
 
 @NgModule({
--- src/app/create-account/create-account.component.ts
+++ src/app/create-account/create-account.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '../../../node_modules/@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { User } from '../models/user';
 import { LoginService } from '../services/login.service';
 
